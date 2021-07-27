4e2de358875ab0979bcfe9d0ac73b08d845ebd4a
arjun596695 <patilmallikarjun596@gmail.com>
2021-07-27 09:08:52 +0530

    My App

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+
+    <div>
+      <h1> Iron Man </h1>
+      <p>
+      Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. 
+      </p>
+      </div>
+  );
+}
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+           <div><h1>Contact</h1>
+           <h1>Assignment-1</h1>
+           <h1>Name: Mallikarjun J Patil</h1>
+           <h1>Id No: 20181CSE9028</h1>
+           </div>
+        </div> 
+    );
+  }
+  
+export default Contact;
